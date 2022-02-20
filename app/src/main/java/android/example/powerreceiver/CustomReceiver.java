c4ec629c00f06ea23743a8da657609d5ba3af9e7
John <jmitropoulos@alumni.berklee.edu>
2022-02-20 00:15:53 -0600

    Finished Basic Power Connect Receiver

--- app/src/main/java/android/example/powerreceiver/CustomReceiver.java
+++ app/src/main/java/android/example/powerreceiver/CustomReceiver.java
@@ -3,13 +3,24 @@ package android.example.powerreceiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.widget.Toast;
 
 public class CustomReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        // TODO: This method is called when the BroadcastReceiver is receiving
-        // an Intent broadcast.
-        throw new UnsupportedOperationException("Not yet implemented");
+        String intentAction = intent.getAction();
+        if (intentAction != null) {
+            String toastMessage = "unknown intent action";
+            switch (intentAction) {
+                case Intent.ACTION_POWER_CONNECTED:
+                    toastMessage = "Powering UP!";
+                    break;
+                case Intent.ACTION_POWER_DISCONNECTED:
+                    toastMessage = "Powering DOWN";
+                    break;
+            }
+            Toast.makeText(context, toastMessage, Toast.LENGTH_SHORT).show();
+        }
     }
 }
