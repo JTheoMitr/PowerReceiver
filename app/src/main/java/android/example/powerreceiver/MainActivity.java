46e05bfb211287c4f73e88e3ba9bdeec192bf0b7
John <jmitropoulos@alumni.berklee.edu>
2022-02-20 20:36:51 -0600

    Added movement tracking to mainactivity

--- app/src/main/java/android/example/powerreceiver/MainActivity.java
+++ app/src/main/java/android/example/powerreceiver/MainActivity.java
@@ -1,16 +1,21 @@
 package android.example.powerreceiver;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.MotionEventCompat;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static  String DEBUG_TAG = MainActivity.class.getSimpleName();
     private CustomReceiver mReceiver = new CustomReceiver();
     private static final String ACTION_CUSTOM_BROADCAST =
             BuildConfig.APPLICATION_ID + ".ACTION_CUSTOM_BROADCAST";
@@ -38,6 +43,35 @@ public class MainActivity extends AppCompatActivity {
     public void sendCustomBroadcast(View view) {
         Intent customBroadcastIntent = new Intent(ACTION_CUSTOM_BROADCAST);
         LocalBroadcastManager.getInstance(this).sendBroadcast(customBroadcastIntent);
+    }
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+
+        int action = event.getActionMasked();
+        String message = "default message";
+
+        switch(action) {
+            case (MotionEvent.ACTION_DOWN) :
+                Log.d(DEBUG_TAG,"Action was DOWN");
+                return true;
+            case (MotionEvent.ACTION_MOVE) :
+                Log.d(DEBUG_TAG,"Action was MOVE");
+                return true;
+            case (MotionEvent.ACTION_UP) :
+                Log.d(DEBUG_TAG,"Action was UP");
+                return true;
+            case (MotionEvent.ACTION_CANCEL) :
+                Log.d(DEBUG_TAG,"Action was CANCEL");
+                return true;
+            case (MotionEvent.ACTION_OUTSIDE) :
+                Log.d(DEBUG_TAG,"Movement occurred outside bounds " +
+                        "of current screen element");
+                return true;
+            default :
+                return super.onTouchEvent(event);
+        }
     }
 
 }
